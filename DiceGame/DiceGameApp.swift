e68d73a5eaede79c1755c8c597caf73f046332eb
David Svensson <davidsvson@gmail.com>
2021-12-16 11:31:09 +0100

    First commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by David Svensson on 2021-12-16.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
